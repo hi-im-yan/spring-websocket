e6946b1bf345c03941d3083766e2b1b0e10b2548
yan <yanajiki2010@hotmail.com>
2024-03-01 22:49:45 -0300

    first commit

---
+++ src/test/java/com/yanajiki/poc/springwebsocket/SpringwebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanajiki.poc.springwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
